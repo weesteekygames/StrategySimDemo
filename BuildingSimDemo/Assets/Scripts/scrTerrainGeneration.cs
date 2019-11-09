90e56b8bce9854c83d9f4b6a8ec88d20e49a5333
weesteekygames <weesteekygames@outlook.com>
2019-11-09 16:51:35 +0000

    feature/terrain-generation: added basic terrain gen

---
+++ BuildingSimDemo/Assets/Scripts/scrTerrainGeneration.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scrTerrainGeneration : MonoBehaviour
+{
+    public const int NUM_SPRITES =1;
+    public SpriteRenderer[] terrain_sprites =  new SpriteRenderer[NUM_SPRITES];
+
+    public int num_tiles_width;
+    public int num_tiles_height;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
